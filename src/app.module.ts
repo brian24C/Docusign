ed70b0d49e89faa0b5f8f5b6b66cac5ebf597607
brian castro <brianjosuecastro@hotmail.com>
2024-10-16 21:19:31 -0500

    actualizar gitignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EnvelopesModule } from './modules/envelopes/envelopes.module';
 
 @Module({
-  imports: [],
+  imports: [EnvelopesModule],
   controllers: [AppController],
   providers: [AppService],
 })
